98c0120e692b17fb4af22a0e551c36b1625aebbb
Anirudh337675 <73932473+Anirudh337675@users.noreply.github.com>
2020-12-24 10:00:07 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,21 @@
+class Bob
+{
+    constructor(x,y,radius)
+    {
+      
+        this.radius=radius
+        this.body=  Matter.Bodies.circle(x,y,radius)
+        World.add(world,this.body);
+    }
+    display()
+    {
+        var roc=this.body.position;
+     
+      
+        ellipseMode(CENTER);
+        fill("purple");
+        ellipse(roc.x,roc.y,this.radius*2,this.radius*2);
+        
+    }
+
+}
